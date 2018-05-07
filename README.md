3e2e54f23d440dfa87ab51780ea9d0c2a0b641ec
Hanai <ihanai1991@gmail.com>
2018-05-07 11:45:22 +0800

    :tada: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# hexo-filter-mermaid-tag
+
+A hexo plugin that covert ```` ``` mermaid```` to `<pre class="mermaid">`.
+
+## Usage
+
+Enable in `_config.yml`:
+
+``` yml
+mermaid:
+  enable: true
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const config = hexo.config.mermaid;
+
+if (config && config.enable) {
+  const filter = require('./lib/filter');
+  hexo.extend.filter.register('before_post_render', filter);
+}
