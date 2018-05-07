8daa820678eb329537b4256357c35da76bcc6547
Hanai <ihanai1991@gmail.com>
2018-05-07 14:14:33 +0800

    :bug: register with priority

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const config = hexo.config.mermaid;
 
 if (config && config.enable) {
   const filter = require('./lib/filter');
-  hexo.extend.filter.register('before_post_render', filter);
+  hexo.extend.filter.register('before_post_render', filter, 9); // default priority is 10
 }
